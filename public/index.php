25bdfb5c1620d3301980eba3345092cef86cc0bf
Mirivlad <mirivlad@gmail.com>
2026-09-02 05:20:32 +0800

    dr: wire maintenance middleware into web runtime

--- public/index.php
+++ public/index.php
@@ -3,7 +3,15 @@
 declare(strict_types=1);
 
 use App\Application\Bootstrap;
+use App\Backup\DrMaintenanceLock;
+use App\Middlewares\DrMaintenanceMiddleware;
+use Slim\Psr7\Factory\ResponseFactory;
 
 require dirname(__DIR__) . '/vendor/autoload.php';
 
-Bootstrap::fromEnvironment()->app()->run();
+$app = Bootstrap::fromEnvironment()->app();
+$app->add(new DrMaintenanceMiddleware(
+    new ResponseFactory(),
+    new DrMaintenanceLock(dirname(__DIR__) . '/var/dr')
+));
+$app->run();
